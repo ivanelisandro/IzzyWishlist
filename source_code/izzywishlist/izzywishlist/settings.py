304289a7eb62957b77984305dfe179c0bea9d7a5
ivan.silva <ivan.elisandro@gmail.com>
2021-02-08 21:10:26 -0300

    CHANGED: Removed old Key for security purposes.

--- source_code/izzywishlist/izzywishlist/settings.py
+++ source_code/izzywishlist/izzywishlist/settings.py
@@ -20,7 +20,7 @@ LOG_FILE = os.path.join(BASE_DIR, 'izzywishlist.logs')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ajqt^$c%bq!@ol#ayt!rrn_g3d#@g3kak*s%v@&dytx#cpn350'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
